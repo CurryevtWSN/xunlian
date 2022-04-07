3ec62d1dda7c188aee8b7f34b8ed6afdf05ae3b0
shimunana <88524372+shimunana@users.noreply.github.com>
2022-04-07 12:43:40 +0800

    Add files via upload

---
+++ definitions.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  5 23:40:12 2022
+
+@author: Administrator
+"""
+
+import pandas as pd
+import streamlit as st
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_breast_cancer
+from sklearn.metrics import roc_auc_score,roc_curve,auc,accuracy_score,classification_report,confusion_matrix,precision_recall_curve
+import lightgbm as lgb
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
+
+def plot_roc(fpr, tpr, label=None):
+    roc_auc = auc(fpr, tpr)
+    plt.title('Receiver Operating Characteristic')
+    plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
+    plt.legend(loc = 'lower right')
+    plt.plot([0, 1], [0, 1],'r--')
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.show()
+    st.pyplot()
